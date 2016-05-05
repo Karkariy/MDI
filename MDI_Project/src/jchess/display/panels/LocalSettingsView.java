158907391757de032d37bf14443651748916ddc4
Karkariy <helali44@hotmail.fr>
2016-05-06 00:12:40 +0200

    init project

---
+++ MDI_Project/src/jchess/display/panels/LocalSettingsView.java
@@ -0,0 +1,171 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jchess.display.panels;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import jchess.core.Game;
+import jchess.utils.Settings;
+
+/**
+ *
+ * @author Mateusz Lach ( matlak, msl )
+ */
+public class LocalSettingsView extends JPanel implements ActionListener
+{
+    private JCheckBox isUpsideDown;
+    
+    private JCheckBox isDisplayLegalMovesEnabled;
+    
+    private JCheckBox isRenderLabelsEnabled;    
+    
+    private JComboBox time4Game ;
+    
+    
+    public final static String TIMES[] =
+        {
+            "1", "3", "5", "8", "10", "15", "20", "25", "30", "60", "120"
+        };
+
+     
+    private GridBagConstraints gbc;
+    
+    private GridBagLayout gbl;
+    
+    private Game game;
+    
+    public LocalSettingsView(Game game)
+    {
+        this.game = game;
+        
+        this.gbc = new GridBagConstraints();
+        this.gbl = new GridBagLayout();
+        
+        this.setLayout(gbl);
+        
+        initUpsideDownControl();
+        initDisplayLegalMovesControl();
+        initRenderLabelsControl();
+        initTimeControl();
+        refreshCheckBoxesState();
+    }
+    
+    private void initUpsideDownControl()
+    {
+        this.isUpsideDown = new JCheckBox();
+        this.isUpsideDown.setText(Settings.lang("upside_down"));
+        this.isUpsideDown.setSize(this.isUpsideDown.getHeight(), this.getWidth());
+        this.gbc.gridx = 0;
+        this.gbc.gridy = 0;
+        this.gbc.insets = new Insets(3, 3, 3, 3);
+        this.gbl.setConstraints(isUpsideDown, gbc);
+        this.add(isUpsideDown);
+        
+        isUpsideDown.addActionListener(this);
+    }
+    
+    private void initDisplayLegalMovesControl()
+    {
+        this.isDisplayLegalMovesEnabled = new JCheckBox();
+        this.isDisplayLegalMovesEnabled.setText(Settings.lang("display_legal_moves"));     
+        
+        this.gbc.gridx = 0;
+        this.gbc.gridy = 1;
+        this.gbl.setConstraints(isDisplayLegalMovesEnabled, gbc);
+        this.add(isDisplayLegalMovesEnabled);
+        
+        isDisplayLegalMovesEnabled.addActionListener(this);        
+    }
+    
+    private void initRenderLabelsControl()
+    {
+        this.isRenderLabelsEnabled = new JCheckBox();
+        this.isRenderLabelsEnabled.setText(Settings.lang("display_labels"));     
+        
+        this.gbc.gridx = 0;
+        this.gbc.gridy = 2;
+        this.gbl.setConstraints(isRenderLabelsEnabled, gbc);
+        this.add(isRenderLabelsEnabled);
+        
+        isRenderLabelsEnabled.addActionListener(this);        
+    }
+    
+    private void initTimeControl()
+    {   
+    	this.time4Game = new JComboBox(TIMES);
+    	
+        this.time4Game.setToolTipText(Settings.lang("Select Time"));     
+        
+        this.gbc.gridx = 0;
+        this.gbc.gridy = 4;
+        this.add(new JLabel("Select Time : "));
+        this.add(time4Game);        
+        time4Game.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				String value = TIMES[time4Game.getSelectedIndex()];//set time for game
+	            Integer val = new Integer(value);
+	            if(game.isFirstTurn()){
+	            game.getSettings().setTimeForGame((int) val* 60);//set time for game and mult it to seconds
+				game.getGameClock().setTimes(game.getSettings().getTimeForGame(),game.getSettings().getTimeForGame());			
+	            }
+			}
+		});
+    }
+    
+
+        
+    private void refreshCheckBoxesState()
+    {
+        if (isInitiatedCorrectly())
+        {
+            isUpsideDown.setSelected(game.getSettings().isUpsideDown());
+            isDisplayLegalMovesEnabled.setSelected(game.getSettings().isDisplayLegalMovesEnabled());
+            isRenderLabelsEnabled.setSelected(game.getSettings().isRenderLabels());
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        JCheckBox clickedComponent = (JCheckBox) e.getSource();
+        if (clickedComponent == isUpsideDown)
+        {
+            game.getSettings().setUpsideDown(isUpsideDown.isSelected());
+        } 
+        else if (clickedComponent == isDisplayLegalMovesEnabled)
+        {
+            game.getSettings().setDisplayLegalMovesEnabled(isDisplayLegalMovesEnabled.isSelected());
+        }
+        else if (clickedComponent == isRenderLabelsEnabled) 
+        {
+            game.getSettings().setRenderLabels(isRenderLabelsEnabled.isSelected());
+            game.resizeGame();
+        }
+        game.repaint();
+    }
+    
+    @Override
+    public void repaint()
+    {
+        refreshCheckBoxesState();
+        super.repaint();
+    }
+
+    private boolean isInitiatedCorrectly()
+    {
+        return null != isUpsideDown && null != isDisplayLegalMovesEnabled
+                && null != isRenderLabelsEnabled;
+    }
+}
